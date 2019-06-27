d74fd34d17c8c00757f224958eaef99fab8814e6
Richard Russon <rich@flatcap.org>
2019-06-28 00:49:35 +0100

    devel: graphviz dump
    Walk though the Account and Mailbox objects and draw a Graphviz diagram
    to represent them.
    
    The `.gv` file is given a timestamp filename.

--- mutt_signal.c
+++ mutt_signal.c
@@ -34,6 +34,7 @@
 #include "globals.h"
 #include "mutt_attach.h"
 #include "mutt_curses.h"
+#include "protos.h"
 #ifdef HAVE_LIBUNWIND
 #include "mutt.h"
 #endif
@@ -102,6 +103,9 @@ static void curses_segv_handler(int sig)
   endwin(); /* just to be safe */
 #ifdef HAVE_LIBUNWIND
   show_backtrace();
+#endif
+#ifdef USE_DEVEL_GRAPHVIZ
+  dump_graphviz("segfault");
 #endif
 
   struct sigaction act;
