c9806d65b4a2926036bc4554954012c85f73e2ea
Richard Russon <rich@flatcap.org>
2019-07-30 13:19:16 +0100

    sidebar: split out config

--- main.c
+++ main.c
@@ -845,6 +845,7 @@ int main(int argc, char *argv[], char *envp[])
   notify_observer_add(Config->notify, NT_CONFIG, 0, mutt_reply_observer, 0);
   if (Colors)
     notify_observer_add(Colors->notify, NT_COLOR, 0, mutt_menu_color_observer, 0);
+  notify_observer_add(Config->notify, NT_CONFIG, 0, mutt_sb_observer, 0);
 
   if (sendflags & SEND_POSTPONED)
   {
